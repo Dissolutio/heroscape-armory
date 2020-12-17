86edfbd38490c5e4329f9ab45d83411fc49261e7
John Moen <entity.john@gmail.com>
2020-12-17 08:13:53 -0600

    add filter when click on army card

--- src/components/DraftArmy.tsx
+++ src/components/DraftArmy.tsx
@@ -2,19 +2,31 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Image from 'react-bootstrap/Image'
+import Badge from 'react-bootstrap/Badge'
 
 import { useUIContext } from 'hooks/useUIContext'
 import { useDeckContext } from 'hooks/useDeckContext'
 import { ArmyDraftCard, useArmySelectContext } from 'hooks/useArmySelectContext'
 
 export const DraftArmy = () => {
-  const { deck } = useDeckContext()
+  const { deck, setFilterForCardID } = useDeckContext()
   const { darkMode, darkModeBSClassNames } = useUIContext()
   const { army, addCardToArmy, removeCardFromArmy } = useArmySelectContext()
+  const armyCost = army.reduce((prev, curr) => {
+    const cardID = curr.cardID
+    const card = deck.find((c) => c.cardID === cardID)
+    const cardPoints = parseInt(card.points)
+    return prev + cardPoints
+  }, 0)
   return (
     <Container className={`mt-2 mb-2`}>
-      <Jumbotron className={`${darkModeBSClassNames} mt-3 pt-1 pb-4`}>
-        <h2 className={`text-center small`}>Army</h2>
+      <Jumbotron
+        className={`${darkModeBSClassNames} text-center mt-3 pt-1 pb-2`}
+      >
+        <h3 className={`d-inline small`}>Army: </h3>
+        <Badge variant="warning" className={`small ml-2 mb-1`}>
+          {armyCost} points
+        </Badge>
         <div className={`d-flex flex-row flex-wrap align-items-center`}>
           {army.map((draftCard: ArmyDraftCard) => {
             const { cardID, count } = draftCard
@@ -23,10 +35,11 @@ export const DraftArmy = () => {
               <div
                 key={cardID}
                 className="d-flex flex-column align-items-center"
+                onClick={() => setFilterForCardID(cardID)}
               >
                 <Image
                   src={`/heroscape-portraits/${armyCard.image}`}
-                  style={{ width: '30px' }}
+                  style={{ width: '50px' }}
                   className="pl-1 pr-1 pt-1"
                 />
                 <span className="small">{`x ${count}`}</span>
